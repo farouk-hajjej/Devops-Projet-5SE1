630f9c0fb6f385266135d4b6915258002da9c27b
farouk-hajjej <74997699+farouk-hajjej@users.noreply.github.com>
2022-10-28 04:01:06 +0200

    new farouk commit 🔧 ✅

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -11,14 +11,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import com.esprit.examen.entities.Stock;
 
 //@RunWith(SpringRunner.class)
